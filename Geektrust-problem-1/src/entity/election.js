83ef5fee57697a4cf6ec7d77f0df6a0fb16f584d
Swayam Siddha <swayam.siddha@bitwiseglobal.com>
2019-08-26 01:41:50 +0530

    starting problem 2

--- Geektrust-problem-1/src/entity/election.js
+++ Geektrust-problem-1/src/entity/election.js
@@ -1,8 +1,23 @@
 
-import { input } from '../config/input'
+import { problem2 } from '../config/input'
 
+
+const claimants = [], nonClaimants = []
 const processedMessageList = []
 
-const pickRandomMessage = () => {
+
+
+const _pickRandomMessage = () => {
    
+}
+
+const _sortKingdomsIntoClaimantsAndNonClaimants = (claimantNames, listOfkingdoms) => {
+    listOfkingdoms.forEach( k => {
+        if(claimantNames.indexOf(k.name.toLowerCase()) > -1 ) claimants.push(k)
+        else nonClaimants.push(k)
+    })
+}
+
+const _processMessages = (messages) => {
+    
 }
